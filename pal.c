4218b18695e4b2e12a7920332bf13f33bca5b965
Cloud User <ec2-user@ip-172-31-23-47.us-east-2.compute.internal>
2021-07-13 09:28:30 +0000

    source code

---
+++ pal.c
@@ -0,0 +1,19 @@
+#include<stdio.h>  
+void pal()    
+{    
+int n,r,sum=0,temp;    
+printf("enter the number=");    
+scanf("%d",&n);    
+temp=n;    
+while(n>0)    
+{    
+r=n%10;    
+sum=(sum*10)+r;    
+n=n/10;    
+}    
+if(temp==sum)    
+printf("its is palindrome number \n");    
+else    
+printf("it is not palindrome\n");   
+//return 0;  
+}   
